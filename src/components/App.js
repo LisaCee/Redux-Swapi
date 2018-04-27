be31df70bef7b9216313f869ccc8ad7953029d55
Lisa Cee <LisaCee@users.noreply.github.com>
2018-04-26 20:31:38 -0700

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import logo from '../logo.svg';
 import '../styles/App.css';
 // pull in actions from action/index
+import { getChars } from '../actions/index';
+
 
 class App extends Component {
   componentDidMount() {
-    // call our action
+    this.props.getChars()
   }
   render() {
     return (
       <div className="App">
+      {this.props.fetching ? <h3>Waiting On the Force</h3>: null}
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
@@ -26,8 +28,15 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    fetched: state.fetched,
+    error: state.error,
+    chars: state.chars
+  }
+}
 // our mapDispatchToProps needs to have two properties inherited from state
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
+export default connect(mapStateToProps, { getChars
 })(App);
