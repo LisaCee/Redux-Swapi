e96619c3869c11a142cd66aa931f6f86d3e2c1c4
Lisa Cee <LisaCee@users.noreply.github.com>
2018-04-26 20:42:30 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // needed dependancies
 // applyMiddleware from redux
+import {applyMiddleware} from 'redux';
 // thunk from redux-thunk
+import thunk from 'redux-thunk';
 // logger from redux-logger
+import logger from 'redux-logger';
 // rootReducer from ./reducers
+import { charsReducer } from './reducers/starWarsReducer';
 
-const store = createStore(/* rootReducer */, /* applyMiddleware goes here */);
+const middleware = applyMiddleware(thunk, logger)
+const store = createStore(charsReducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
